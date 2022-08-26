bea1b5479135f46b0346c3f8f4e45360bdb2959f
buffetng <ulaitorcodes@gmail.com>
2022-08-26 15:11:24 +0100

    functions and views, first commit

---
+++ init.py
@@ -0,0 +1,23 @@
+def transform_image(image_bytes):
+    """
+    Transform image into required DenseNet format: 224x224 with 3 RGB channels and normalized.
+    Return the corresponding tensor.
+    """
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+
+
+def get_prediction(image_bytes):
+    """For given image bytes, predict the label using the pretrained DenseNet"""
+    tensor = transform_image(image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    class_name, human_label = imagenet_mapping[predicted_idx]
+    return human_label
